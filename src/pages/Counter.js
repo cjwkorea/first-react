b353e1ef900e1278156c188430fccfa04e49b598
cjwkorea <cjwkorea0@gmail.com>
2022-11-05 17:55:35 +0900

    first

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -5,7 +5,9 @@ import Footer from "components/commons/Footer";
 
 const Counter = () => {
   const navigate = useNavigate();
-
+  const shoot = () => {
+    alert("잘했어!");
+  }
   const [num, setNum] = useState(0);
 
   const increase = () => setNum((prev) => prev + 1);
@@ -14,10 +16,17 @@ const Counter = () => {
     <HeadlessLayout>
       <div>
       <div>카운터페이지</div>
-      
+      <button onClick={shoot}>눌러봐.</button>
+      <form>
+      <label>이름:
+        <input type="text" />
+      </label>
+    </form>
       <div>클릭수 {num}</div>
+      
       <div>
         <button onClick={increase}>클릭</button>
+        
       </div>
       
       {/* <Link to-{"/"}> 메인으로 이동하는 페이지*/ }
